ee23a744abb5c41e5afb838aa561fcbd92870ae4
abhiram <abhiramkonda2004@gmail.com>
2024-09-24 20:18:39 +0530

    Added new folders

--- 67_111/src/Components/Counter.jsx
+++ 67_111/src/Components/Counter.jsx
@@ -3,22 +3,15 @@ import { useState } from 'react';
 const Counter = () => {
   // Declare a state variable 'count' and a setter 'setCount'
   const [count, setCount] = useState(0);
-
-  // Function to increment count
   const increment = () => {
     setCount(count + 1);
   };
-
-  // Function to decrement count
   const decrement = () => {
     setCount(count - 1);
   };
-
-  // Function to reset count to 0
   const reset = () => {
     setCount(0);
   };
-
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
       <h1>Counter: {count}</h1>
